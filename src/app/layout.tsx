5a0d8e380f39b960ccefa8b74c51d36e6627686d
abdusSamadJIMS <abdussamadjims@gmail.com>
2024-04-02 00:25:32 +0530

    addMicroData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from 'next/font/local'
 import "./globals.css";
-import { NextFont } from "next/dist/compiled/@next/font";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Providers from "@/components/Providers";
 import { getPhoneOnly } from "@/lib/actions";
+import { jsonLd } from "@/lib/json-ld";
 
 const inter = Inter({ subsets: ["latin"] });
 const agatho = localFont({
@@ -36,7 +36,8 @@ export const metadata: Metadata = {
     url: "https://johannabeautysalon.in",
   },
   keywords: ['beauty', 'fashion', 'beautiful', 'acne'],
-  verification: { google: 'bKHS0Re-QcRIZkH3UYplJ5AxSR9LwMDJB-9SleG8JAU' }
+  verification: { google: 'bKHS0Re-QcRIZkH3UYplJ5AxSR9LwMDJB-9SleG8JAU' },
+
 };
 
 export default async function RootLayout({
@@ -49,10 +50,13 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${agatho.variable}`}>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         <Providers>
           <Header />
           {children}
-
           <Footer phoneNumber={phoneNumber} />
         </Providers>
       </body>
---
+++ src/lib/json-ld.ts
@@ -0,0 +1,23 @@
+export const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'BeautySalon',
+    name: "Johanna Beauty Salon",
+    url: "https://johannabeautysalon.in",
+    sameAs: [
+        "https://www.johannabeautysalon.in"
+    ],
+    logo: "https://johannabeautysalon.in/static/logo/logoJohanna.png",
+    priceRange: "$$",
+    description: "Johanna beauty Salon is a beauty salon situated in New Delhi. Having verity of services related to beauty and makeover. There range goes from cleansing to bridal makeups",
+    address: {
+        ' @type': "PostalAddress",
+        streetAddress: "10/45 Geeta Colony",
+        addressLocality: "New Delhi",
+        addressRegion: "Delhi",
+        postalCode: '110031',
+        addressCountry: "India"
+    },
+    hasMap: "https://maps.app.goo.gl/P164MLADdSJJiTjf8",
+    openingHours: "Mo 10:30-20:00 We 10:30-20:00 Th 10:30-20:00 Fr 10:30-20:00 Sa 10:30-20:00 Su 10:30-20:00",
+    telephone: "+919990049326"
+}
